d22db4d2307ebbdffc82d647f2311fc68d2170be
duytq <duybkit13@gmail.com>
2021-10-26 09:57:28 +0700

    integrate viper go gin config

---
+++ api/configurations/config.go
@@ -0,0 +1,25 @@
+package configurations
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (Config, error) {
+	var config Config
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return config, err
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
